5cc5eaaba84102c9d25a2eb9ee61fecaeada897b
yzy613 <59520517+yzy613@users.noreply.github.com>
2020-07-27 14:53:24 +0800

    upload

---
+++ README.md
@@ -0,0 +1,22 @@
+# 我的世界服务器 Bash 脚本
+快速管理你的我的世界服务器。依赖 screen 工具，请先提前安装。
+
+# 用法
+- 不使用 screen 运行，直接运行（一般用于测试服务器）
+```bash
+./operate_service.sh direct
+```
+- 启动服务器
+```bash
+./operate_service.sh start
+```
+
+- 停止服务器
+```bash
+./operate_service.sh stop
+```
+
+- 重启服务器
+```bash
+./operate_service.sh restart
+```
---
+++ operate_service.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# Latest edit : 2020/01/22
+# Code by yzy
+
+USERNAME='root'
+BIN_PATH='/opt/minecraft/1.15.2'
+BOOT_FILE='server.jar'
+NAME='Minecraft-Java'
+MAX_MEM='2048M'
+MIN_MEM='1024M'
+LOG="$BIN_PATH/logs/record_status.log"
+
+WAITING_TIME=5
+
+ME=`whoami`
+
+
+if [ $ME != $USERNAME ] ; then
+	echo "Please run the $USERNAME user."
+	exit
+fi
+
+direct() {
+	cd $BIN_PATH
+	java -Xmx$MAX_MEM -Xms$MIN_MEM -jar $BIN_PATH/$BOOT_FILE nogui
+}
+
+start() {
+	if pgrep -u $USERNAME -f $NAME > /dev/null ; then
+		echo "$NAME is already running"
+		exit
+	fi
+
+	echo "Starting $NAME..."
+
+	cd $BIN_PATH
+	screen -AmdS $NAME java -Xmx$MAX_MEM -Xms$MIN_MEM -jar $BIN_PATH/$BOOT_FILE nogui
+
+	echo $(date +%Y/%m/%d-%H:%M)_Start $NAME >> $LOG
+	exit
+}
+
+stop() {
+	if pgrep -u $USERNAME -f $NAME > /dev/null ; then
+		echo "$NAME stopped"
+	else
+		echo "$NAME is not running"
+		exit
+	fi
+
+	#screen -S $NAME -X quit
+	screen -S $NAME -X eval 'stuff "stop"\015'
+
+	echo $(date +%Y/%m/%d-%H:%M)_Stop $NAME >> $LOG
+	exit
+}
+
+restart() {
+	if pgrep -u $USERNAME -f $NAME > /dev/null ; then
+		#screen -S $NAME -X quit
+	screen -S $NAME -X eval 'stuff "stop"\015'
+	fi
+
+	echo "Please wait $WAITING_TIME seconds for restarting"
+	sleep $WAITING_TIME
+
+	cd $BIN_PATH
+	screen -AmdS $NAME java -Xmx$MAX_MEM -Xms$MIN_MEM -jar $BIN_PATH/$BOOT_FILE nogui
+	echo "$NAME restarted"
+
+	echo $(date +%Y/%m/%d-%H:%M)_Restart $NAME >> $LOG
+	exit
+}
+
+status() {
+	if pgrep -u $USERNAME -f $NAME > /dev/null ; then
+		echo "$NAME is already running"
+		exit
+	else
+		echo "$NAME is not running"
+		exit
+	fi
+}
+
+case "$1" in
+	start)
+		start
+		;;
+	stop)
+		stop
+		;;
+	restart)
+		restart
+		;;
+	status)
+		status
+		;;
+	direct)
+		direct
+		;;
+	*)
+		echo "Usage: $0 [start/stop/restart/status/direct]"
+esac
